4c19d5577ee10ee25d2b877a7aae8845925a3ae4
Guillaume Belouin <gbelouin@protonmail.com>
2022-09-17 11:43:03 +0200

    Remove the payment lib

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,10 @@ import 'package:chic_secret/provider/theme_provider.dart';
 import 'package:chic_secret/ui/screen/landing_screen.dart';
 import 'package:chic_secret/utils/database.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 import 'package:provider/provider.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -21,11 +19,6 @@ Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   timeago.setLocaleMessages('fr', timeago.FrMessages());
   await initDatabase();
-
-  // Enable purchases for Android
-  if (defaultTargetPlatform == TargetPlatform.android) {
-    InAppPurchaseAndroidPlatformAddition.enablePendingPurchases();
-  }
 
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
